cceef2c01e4af49b11dc972de13c7adcbcc0bf1a
shivansh-123hub <shiv2006sharma@gmail.com>
2026-01-11 18:56:37 +0530

    Add basic pattern printing programs using nested loops
    Implement a pattern printing program using nested loops.

---
+++ Basics/Patterns/Pattern1.java
@@ -0,0 +1,13 @@
+// Pattern printing program
+// Concept used: Nested loops
+public class Basic{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    for(int line =1;line<=4;line++){
+      for(int star=1;star<=line;star++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
