1eeae8a356c3c5a6a35044e9f9b37fa30a6fe332
shivansh-123hub <shiv2006sharma@gmail.com>
2026-01-11 19:09:31 +0530

    Add basic star  pattern program using  nested loops

--- Basics/Patterns/Pattern1.java
+++ Basics/Patterns/Pattern1.java
@@ -1,6 +1,6 @@
 //  Star Pattern printing program
 // Concept used: Nested loops
-public class Basic{
+public class Pattern1{
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     for(int line =1;line<=4;line++){
