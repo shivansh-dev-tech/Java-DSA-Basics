d58ce0e8e2d396fc415287e03d03261ec7372460
shivansh-123hub <shiv2006sharma@gmail.com>
2026-01-11 19:08:27 +0530

    Add inverted star pattern using nested loops

---
+++ Basics/Patterns/Pattern2.java
@@ -0,0 +1,13 @@
+// Pattern: Inverted Star Pattern
+// Concept: Nested loops
+public class Pattern2 {
+  public static void main(String[] args){
+  int n=4; 
+    for(int line=1;line<=4;line++){
+      for(int star=1;star<=n-line+1;star++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+}
