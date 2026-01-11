78ccb4b828f4cceb1443aa44a5c7f31b89d06dd7
shivansh-123hub <shiv2006sharma@gmail.com>
2026-01-11 18:31:43 +0530

    Added reverse number program

---
+++ Basics/ReverseNumber.java
@@ -0,0 +1,14 @@
+// Reverse a number using loops
+// Concepts used: while loop, modulo operator
+public class Basic{
+  public static void main(String[] args) {
+    int n = 4556;
+    int rev =0;
+    while (n>0) {
+      int lastDigit = n%10; 
+      rev = (rev*10) + lastDigit;
+      n /=10;
+    }
+    System.out.println(rev);
+  }
+}
